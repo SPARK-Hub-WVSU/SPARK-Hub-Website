01267fa0b6424be7f6088f2745ee6a53ac98e74f
renz0826 <brenzbedonia@gmail.com>
2025-08-10 00:34:40 +0800

    Resolved adjustments and suggestions. Added animations

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
-import Forms from "@/components/layout/ui/Forms";
+import Forms from "@/components/ui/Footer_Forms";
 
 export default function Footer() {
   return (
-    <footer className="bg-primary-dark">
+    <footer className="bg-[url('/bg/footer_bg.png')] bg-cover bg-center bg-no-repeat w-full min-h-screen">
 
       <nav className="text-white text-xl w-auto h-auto flex flex-row justify-between py-20 px-36">
         
         <div id="Logo" className="flex flex-col gap-6">
-          <img src="/vectors/footer_logo.svg"/>
+          <div className="flex items-center gap-6">
+            <img className="" src="/vectors/footer_logo.svg"/> 
+            <div className="font-bold text-5xl flex gap-5">
+              <h1 className="bg-gradient-to-r from-yellow-400 to-yellow-500 bg-clip-text text-transparent">SPARK</h1> 
+              <h1 className="bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">HUB</h1>
+            </div>
+          </div>
           <p className="font-medium"> 
             IGNITING INNOVATION <br></br> INNOVATING THE FUTURE
           </p>
@@ -17,18 +23,18 @@ export default function Footer() {
         <div id="Links" className="flex flex-row gap-20 justify-center max-w-xl">
 
           <div id="Home" className="flex flex-col gap-4">
-            <div className="font-semibold">
+            <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
               <Link href=""> Home </Link>
             </div>
               <div className="flex flex-col gap-4">
-                <Link href="" > Mission </Link>
-                <Link href="" > Vision </Link>
-                <Link href=""> Recent <br></br>News</Link>
+                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white" > Mission </Link>
+                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Vision </Link>
+                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Recent <br></br>News</Link>
               </div>
           </div>
 
           <div id="About" className="flex flex-col gap-4">
-            <div className="font-semibold">
+            <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
               <Link href=""> About Us </Link>
             </div>
               <div className="flex flex-col gap-4">
@@ -38,7 +44,7 @@ export default function Footer() {
           </div>
 
           <div id="Contact" className="flex flex-col gap-4">
-            <div className="font-semibold">
+            <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
               <Link href=""> Contacts </Link>
             </div>
               <div className="flex flex-col gap-4">
@@ -48,7 +54,7 @@ export default function Footer() {
           </div>
 
           <div id="Team" className="flex flex-col gap-4">
-            <div className="font-semibold">
+            <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
               <Link href=""> Team </Link>
             </div>
           </div>
@@ -73,13 +79,21 @@ export default function Footer() {
 
       </section>
 
-      <section id="Copy_Right" className="flex flex-row justify-between items-center mx-36 mb-40">
+      <section id="Copy_Right" className="flex flex-row justify-between items-center px-36 pb-40">
         <p className="text-white font-medium text-xl"> © 2025 SPARK HUB. All rights reserved</p>
 
         <div id="FB_GM" className="flex flex-row gap-10">
-          <Link href=""> <img src="/icons/facebook.svg" className="shadow-2xl"/></Link>
-          <Link href=""> <img src="/icons/gmail.svg" className="shadow-2xl"/></Link>
+          <Link href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"> 
+            <img src="/icons/facebook.svg" className="absolute z-10 shadow-inner"/>
+            <img src="/icons/facebook.svg" className="relative saturate-0 brightness-0 blur-md opacity-80"/>
+          </Link>
+          <Link href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"> 
+            <img src="/icons/gmail.svg" className="absolute z-10 shadow-inner"/>
+            <img src="/icons/gmail.svg" className="absolute z-0 saturate-0 brightness-0 blur-md"/>
+            <img src="/icons/gmail.svg" className="relative z-0 saturate-0 brightness-0 blur-md opacity-10"/>
+          </Link>
         </div>
+
       </section>
 
 
--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import ContactBtn from "@/components/layout/ui/ContactBtn";
 
 export default function Header() {
   return (
@@ -7,15 +6,28 @@ export default function Header() {
       <nav className="text-white text-2xl w-full h-auto flex flex-row items-center justify-between py-3 px-36">
           
           <div id="Home" className="mb-2">
-            <Link href="/">
-              <img src="/vectors/nav_logo.svg" alt = "SPARK Hub Logo"/>
+            <Link href="" className="font-semibold text-3xl flex items-center gap-8">
+              <img className="absolute" src="/vectors/nav_logo.svg" alt = "SPARK Hub Logo"/>
+              <img id="Logo_Glow" className="relative blur-sm opacity-60" src="/vectors/nav_logo.svg" alt = "SPARK Hub Logo"/> 
+              SPARK HUB
             </Link>
           </div>
 
           <div id="Content"className="flex flex-row gap-10 justify-center items-center">
-            <Link href="/about">About Us</Link>
-            <Link href="/team"> Team</Link>
-            <Link href=""><ContactBtn /></Link>
+            <Link href="/about" className="transform transition-all duration-300 ease-in-out hover:glow-2xl hover:glow-white"> About Us </Link>
+            <Link href="/team" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Team </Link>
+            <Link id="Contact" href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105">
+              <button className="text-accent bg-white bg-opacity-5 backdrop-blur-3xl w-auto px-7 py-5 flex flex-row items-center gap-3 border rounded-3xl">
+                <div>
+                  Contact Us
+                </div>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="4" stroke="currentColor" className="size-6 items-center">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                  </svg>
+                </div>
+              </button>
+            </Link>
           </div>
 
         </nav>
