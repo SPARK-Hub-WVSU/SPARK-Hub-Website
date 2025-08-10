20f08db1d5ab2c51c703d3141d604c2740e0764b
Manns <carado.johnp11q@gmail.com>
2025-08-11 07:14:37 +0800

    fix: Redefine proper routing to pages
    For future improvements, we should use Next's `Image` component for all `<img>` tags for automatic image optimization.

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -4,100 +4,163 @@ import Forms from "@/components/ui/Footer_Forms";
 export default function Footer() {
   return (
     <footer className="bg-[url('/bg/footer_bg.png')] bg-cover bg-center bg-no-repeat w-full min-h-screen">
-
       <nav className="text-white text-xl w-auto h-auto flex flex-row justify-between py-20 px-36">
-        
         <div id="Logo" className="flex flex-col gap-6">
           <div className="flex items-center gap-6">
-            <img className="" src="/vectors/footer_logo.svg"/> 
+            <img className="" src="/vectors/footer_logo.svg" />
             <div className="font-bold text-5xl flex gap-5">
-              <h1 className="bg-gradient-to-r from-yellow-400 to-yellow-500 bg-clip-text text-transparent">SPARK</h1> 
-              <h1 className="bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">HUB</h1>
+              <h1 className="bg-gradient-to-r from-yellow-400 to-yellow-500 bg-clip-text text-transparent">
+                SPARK
+              </h1>
+              <h1 className="bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">
+                HUB
+              </h1>
             </div>
           </div>
-          <p className="font-medium"> 
+          <p className="font-medium">
             IGNITING INNOVATION <br></br> INNOVATING THE FUTURE
           </p>
         </div>
 
-        <div id="Links" className="flex flex-row gap-20 justify-center max-w-xl">
-
+        <div
+          id="Links"
+          className="flex flex-row gap-20 justify-center max-w-xl"
+        >
           <div id="Home" className="flex flex-col gap-4">
             <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
-              <Link href=""> Home </Link>
+              <Link href="/"> Home </Link>
             </div>
-              <div className="flex flex-col gap-4">
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white" > Mission </Link>
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Vision </Link>
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Recent <br></br>News</Link>
-              </div>
+            <div className="flex flex-col gap-4">
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Mission
+              </Link>
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Vision
+              </Link>
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Recent <br></br>News
+              </Link>
+            </div>
           </div>
 
           <div id="About" className="flex flex-col gap-4">
             <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
-              <Link href=""> About Us </Link>
+              <Link href="/about"> About Us </Link>
             </div>
-              <div className="flex flex-col gap-4">
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white" > Section </Link>
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Section </Link>
-              </div>
+            <div className="flex flex-col gap-4">
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Section
+              </Link>
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Section
+              </Link>
+            </div>
           </div>
 
           <div id="Contact" className="flex flex-col gap-4">
             <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
               <Link href=""> Contacts </Link>
             </div>
-              <div className="flex flex-col gap-4">
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white" > Facebook </Link>
-                <Link href="" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Email </Link>
-              </div>
+            <div className="flex flex-col gap-4">
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Facebook
+              </Link>
+              <Link
+                href=""
+                className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+              >
+                Email
+              </Link>
+            </div>
           </div>
 
           <div id="Team" className="flex flex-col gap-4">
             <div className="font-semibold transform transition-all duration-300 ease-in-out  hover:glow-md hover:glow-white">
-              <Link href=""> Team </Link>
+              <Link href="/officers"> Team </Link>
             </div>
           </div>
-
         </div>
-
       </nav>
 
-      <hr id="Line" className="flex border-t-1 border-white border-opacity-40 py-12 mx-36" >
-      </hr>
+      <hr
+        id="Line"
+        className="flex border-t-1 border-white border-opacity-40 py-12 mx-36"
+      ></hr>
 
       <section id="Connect" className="flex flex-row justify-between px-36">
-
         <div id="Stay" className="flex flex-col gap-8">
-          <h1 className="text-accent text-6xl font-semibold"> Stay in <br></br> Touch!</h1>
-          <p className= "text-white">
+          <h1 className="text-accent text-6xl font-semibold">
+            {" "}
+            Stay in <br></br> Touch!
+          </h1>
+          <p className="text-white">
             Ready to spark innovation? <br></br> Get in Touch!
           </p>
         </div>
 
-        <Forms/>
-
+        <Forms />
       </section>
 
-      <section id="Copy_Right" className="flex flex-row justify-between items-center px-36 pb-40">
-        <p className="text-white font-medium text-xl"> © 2025 SPARK HUB. All rights reserved</p>
+      <section
+        id="Copy_Right"
+        className="flex flex-row justify-between items-center px-36 pb-40"
+      >
+        <p className="text-white font-medium text-xl">
+          {" "}
+          © 2025 SPARK HUB. All rights reserved
+        </p>
 
         <div id="FB_GM" className="flex flex-row gap-10">
-          <Link href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"> 
-            <img src="/icons/facebook.svg" className="absolute z-10 shadow-inner"/>
-            <img src="/icons/facebook.svg" className="relative saturate-0 brightness-0 blur-md opacity-80"/>
+          <Link
+            href=""
+            className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"
+          >
+            <img
+              src="/icons/facebook.svg"
+              className="absolute z-10 shadow-inner"
+            />
+            <img
+              src="/icons/facebook.svg"
+              className="relative saturate-0 brightness-0 blur-md opacity-80"
+            />
           </Link>
-          <Link href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"> 
-            <img src="/icons/gmail.svg" className="absolute z-10 shadow-inner"/>
-            <img src="/icons/gmail.svg" className="absolute z-0 saturate-0 brightness-0 blur-md"/>
-            <img src="/icons/gmail.svg" className="relative z-0 saturate-0 brightness-0 blur-md opacity-10"/>
+          <Link
+            href=""
+            className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"
+          >
+            <img
+              src="/icons/gmail.svg"
+              className="absolute z-10 shadow-inner"
+            />
+            <img
+              src="/icons/gmail.svg"
+              className="absolute z-0 saturate-0 brightness-0 blur-md"
+            />
+            <img
+              src="/icons/gmail.svg"
+              className="relative z-0 saturate-0 brightness-0 blur-md opacity-10"
+            />
           </Link>
         </div>
-
       </section>
-
-
     </footer>
-
   );
 }
--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,38 +1,63 @@
 import Link from "next/link";
-import {
-  ArrowRightIcon
-} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
     <header className="bg-primary-dark">
       <nav className="text-white text-2xl w-full h-auto flex flex-row items-center justify-between py-3 px-36">
-          
-          <div id="Home" className="mb-2">
-            <Link href="" className="font-semibold text-3xl flex items-center gap-8">
-              <img className="absolute" src="/vectors/nav_logo.svg" alt = "SPARK Hub Logo"/>
-              <img id="Logo_Glow" className="relative blur-sm opacity-60" src="/vectors/nav_logo.svg" alt = "SPARK Hub Logo"/> 
-              <h1> SPARK HUB </h1>
-            </Link>
-          </div>
+        <div id="Home" className="mb-2">
+          <Link
+            href="/"
+            className="font-semibold text-3xl flex items-center gap-8"
+          >
+            <img
+              className="absolute"
+              src="/vectors/nav_logo.svg"
+              alt="SPARK Hub Logo"
+            />
+            <img
+              id="Logo_Glow"
+              className="relative blur-sm opacity-60"
+              src="/vectors/nav_logo.svg"
+              alt="SPARK Hub Logo"
+            />
+            <h1> SPARK HUB </h1>
+          </Link>
+        </div>
 
-          <div id="Content"className="flex flex-row gap-10 justify-center items-center">
-            <Link href="/about" className="transform transition-all duration-300 ease-in-out hover:glow-2xl hover:glow-white"> About Us </Link>
-            <Link href="/team" className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"> Team </Link>
-            <Link id="Contact" href="" className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105">
-              <button className="text-accent bg-white bg-opacity-5 backdrop-blur-3xl w-auto px-7 py-5 flex flex-row items-center gap-3 border rounded-3xl">
-                <div>
-                  Contact Us
-                </div>
-                <div>
-                  <ArrowRightIcon strokeWidth="4" className="mx-1 h-6 stroke-accent"/>
-                </div>
-              </button>
-            </Link>
-          </div>
-
-        </nav>
+        <div
+          id="Content"
+          className="flex flex-row gap-10 justify-center items-center"
+        >
+          <Link
+            href="/about"
+            className="transform transition-all duration-300 ease-in-out hover:glow-2xl hover:glow-white"
+          >
+            About Us
+          </Link>
+          <Link
+            href="/officers"
+            className="transform transition-all duration-300 ease-in-out  hover:glow-2xl hover:glow-white"
+          >
+            Team
+          </Link>
+          <Link
+            id="Contact"
+            href=""
+            className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"
+          >
+            <button className="text-accent bg-white bg-opacity-5 backdrop-blur-3xl w-auto px-7 py-5 flex flex-row items-center gap-3 border rounded-3xl">
+              <div>Contact Us</div>
+              <div>
+                <ArrowRightIcon
+                  strokeWidth="4"
+                  className="mx-1 h-6 stroke-accent"
+                />
+              </div>
+            </button>
+          </Link>
+        </div>
+      </nav>
     </header>
-
   );
 }
