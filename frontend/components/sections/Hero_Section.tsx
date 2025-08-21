e2885c46e9ae3a32153d047893b2fc55ab5215e9
renz0826 <brenzbedonia@gmail.com>
2025-08-21 09:18:35 +0800

    Hero Section Responsiveness

--- frontend/components/sections/Hero_Section.tsx
+++ frontend/components/sections/Hero_Section.tsx
@@ -2,86 +2,101 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section id ="Hero_Section" className="bg-[url('/bg/hero_bg.png')] bg-cover bg-center bg-no-repeat max-w-full min-h-screen -mt-32 pt-96">
-        {/* Hero */}
-        <div id="Title"
-             className="flex flex-col items-center px-36 -mt-60
-             ">
-          
-          {/* Upper-Part */}
-          <div id="Upper_Part"
-               className="grid grid-cols-3 place-items-center max-w-5xl"
+    <section
+      id="Hero_Section"
+      className="md:bg-[url('/bg/hero_bg.png')] bg-[url('/bg/mobile_hero_bg.png')] bg-cover bg-center bg-no-repeat md:max-w-full min-h-screen md:-mt-32 -mt-40 md:pt-96 pt-36 flex flex-col items-center justify-center"
+    >
+      {/* Hero */}
+      <div
+        id="Title"
+        className="flex flex-col items-center md:px-36 px-9 md:-mt-60 md:max-w-full max-w-[360px]"
+      >
+        {/* Upper-Part */}
+        <div
+          id="Upper_Part"
+          className="grid grid-cols-3 place-items-center md:max-w-5xl"
+        >
+          <Image
+            height={290}
+            width={290}
+            src="/vectors/wifi.svg"
+            alt="wifi"
+            className="opacity-65 mt-32 md:mr-20 mr-9 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
+          />
+
+          <div
+            id="Logo"
+            className="relative md:w-[120px] md:h-[150px] w-[56px] h-[72px] "
           >
             <Image
-              height={290}
-              width={290}
-              src="/vectors/wifi.svg"
-              alt="wifi"
-              className="opacity-65 mt-32 mr-20 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
-            />
-
-            <div id="Logo">
-              <Image
-                width={120}
-                height={120}
-                src="/vectors/hero_logo.svg"
-                alt="SPARK HUB Logo"
-                className="flex mt-48 transition-all duration-300 ease-in-out hover:glow-md hover:glow-amber-300"
-              />  
-            </div>
-
-            <Image
-              height={400}
-              width={400}
-              src="/vectors/computer.svg"
-              alt="computer"
-              className="opacity-65 mr-60 m-4 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
+              src="/vectors/hero_logo.svg"
+              alt="SPARK HUB Logo"
+              fill
+              className="md:mt-24 mt-16 transition-all duration-300 ease-in-out hover:glow-md hover:glow-amber-300"
             />
           </div>
 
-          {/* Spark Typography */}
-          <div id="SPARK_Typorgraphy" className="font-bold text-9xl flex gap-5 -mt-5 mb-16">
-            <h1 className="bg-gradient-to-r from-yellow-400 to-yellow-500 bg-clip-text text-transparent">
-              SPARK
-            </h1>
-            <h1 className="bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">
-              HUB
-            </h1>
-          </div>
+          <Image
+            height={400}
+            width={400}
+            src="/vectors/computer.svg"
+            alt="computer"
+            className="opacity-65 md:mr-60 mr-[3.5rem] m-4 md:mt-0 mt-20 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
+          />
+        </div>
 
-          {/* Tag Line */}
-          <div id="Tag Line">
-            <div className="text-white text-3xl flex flex-row gap-4">
-              <h1 className="font-bold"> 
-                CREATE 
-              </h1> — 
-              <h1 className="font-bold"> 
-                COLLABORATE
-              </h1> — 
-              <h1 className="font-bold"> 
-                EMPOWER
-              </h1>
-            </div>
-          </div>
+        {/* Spark Typography */}
+        <div
+          id="SPARK_Typorgraphy"
+          className="font-bold md:text-9xl text-5xl flex gap-5 -mt-5 md:mb-16 mb-3"
+        >
+          <h1 className="bg-gradient-to-r from-yellow-400 to-yellow-500 bg-clip-text text-transparent">
+            SPARK
+          </h1>
+          <h1 className="bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">
+            HUB
+          </h1>
+        </div>
 
-          {/* Lower-Part */}
-          <div id="Lower_Part"
-               className="grid grid-cols-3 place-items-center max-w-6xl mb-72"
-          >
+        {/* Tag Line */}
+        <div id="Tag Line">
+          <div className="text-white md:text-3xl text-[12px] flex flex-row md:gap-4 gap-2">
+            <h1 className="font-bold">CREATE</h1> —
+            <h1 className="font-bold">COLLABORATE</h1> —
+            <h1 className="font-bold">EMPOWER</h1>
+          </div>
+        </div>
+
+        {/* Lower-Part */}
+        <div
+          id="Lower Part"
+          className="grid grid-cols-3 md:max-w-[74.5rem] max-w-2xl"
+        >
+          <div>
             <Image
               height={290}
               width={290}
               src="/vectors/people.svg"
-              alt="wifi"
-              className="opacity-65 mr-32 -mt-5 ml-1 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
+              alt="people"
+              className="md:opacity-65 opacity-0 md:mr-[33rem] md:-mt-5 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
             />
-               
-            <h2 className="text-xl text-white italic">
+
+            <Image
+              height={290}
+              width={290}
+              src="/vectors/mobile_people.svg"
+              alt="wifi"
+              className="md:opacity-0 opacity-65 -mt-[6.3rem] md:-ml-[3.8rem] -ml-[3.2rem] scale-50 transition-all duration-300 ease-in-out hover:opacity-100 hover:glow-sm hover:glow-white"
+            />
+          </div>
+
+          <div className="flex justify-center md:mb-80 mb-96">
+            <h2 className="md:text-xl text-[0.75rem] md:mt-14 mt-4 flex text-center text-white italic md:relative absolute md:w-fit w-32">
               Turning ideas into impactful solutions
             </h2>
           </div>
-
         </div>
+      </div>
     </section>
   );
 }
