c91b2446fc97a785f67a0b909a48a59c8ec661db
Renz Bedonia <brenzbedonia@gmail.com>
2025-09-24 13:28:00 +0800

    refactor: used mapping and code cleanup
    Changes:
    - Transferred tailwind classes to mission and vision BG component
    - Used mapping for decluttering homepage file

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 import Hero from "@/components/sections/Hero_Section";
 import Home_About from "@/components/sections/Home_About";
 import Image from "next/image";
-import { Circle } from "@/components/ui/Vision_Mission_Circle";
+import { VisionCircles } from "@/components/ui/Mission_Vision_BG";
+import { MissionCircles } from "@/components/ui/Mission_Vision_BG";
 
 export default function Home() {
   return (
@@ -22,35 +23,7 @@ export default function Home() {
         className="md:py-96 py-48 lg:px-36 sm:px-16 px-9 text-slate-900 flex flex-col items-center justify-center"
       >
         {/* Background Circles */}
-        <div className="relative">
-          <div className="absolute md:-top-[23rem] -top-[7rem] md:left-[23rem] left-[8rem]">
-            <Circle className="md:w-[470px] w-[160px] md:h-[470px] h-[160px]"/>
-          </div>
-          <div className="absolute md:top-[15rem] top-[4rem] md:-left-[13rem]">
-            <Circle className="md:w-[465px] w-[105px] md:h-[465px] h-[105px]"/> 
-          </div>
-          <div className="absolute md:-top-[5rem] top-[0.25rem] md:left-[8rem] left-[4rem]">
-            <Circle className="md:w-[245px] w-[58px] md:h-[245px] h-[58px]"/> 
-          </div>
-          <div className="absolute md:top-[16rem] top-[3rem] md:left-[18rem] left-[8rem]">
-            <Circle className="md:w-[160px] w-[48px] md:h-[160px] h-[48px]"/>
-          </div>
-          <div className="absolute md:block hidden md:top-[25rem] md:left-[29rem]">
-            <Circle className="md:w-[100px] w-[105px] md:h-[100px] h-[105px]"/> 
-          </div>
-          <div className="absolute md:top-[28rem] top-[19rem] md:right-[28rem] right-[8rem]">
-            <Circle variant="teal" className="md:w-[470px] w-[350px] md:h-[470px] h-[350px]" /> 
-          </div>
-          <div className="absolute md:top-[18rem] top-[25rem] md:right-[15rem] -right-[3rem]">
-            <Circle variant="teal" className="md:w-[265px] w-[120px] md:h-[265px] h-[120px]"/>
-          </div>
-          <div className="absolute md:-top-[5rem] top-[15rem] md:right-[10rem] right-[6rem]">
-            <Circle variant="teal" className="md:w-[200px] w-[75px] md:h-[200px] h-[75px]" /> 
-          </div>
-          <div className="absolute md:top-[10rem] top-[6rem] md:right-[32rem] right-[3rem]">
-            <Circle variant="teal" className="md:w-[130px] w-[55px] md:h-[130px] h-[55px]"/> 
-          </div>
-        </div>
+        <MissionCircles />
 
         <div
           id="Our_Mission"
@@ -157,33 +130,9 @@ export default function Home() {
         id="Vision"
         className="md:py-96 py-48 lg:px-36 sm:px-16 px-9 text-slate-900 flex flex-col items-center justify-center"
       >
-          {/* Background Circles */}
-          <div className="relative">
-            <div className="absolute md:top-[4rem] top-[4rem] md:-left-[7rem] -left-[1rem]">
-              <Circle className="md:w-[414px] w-[110px] md:h-[414px] h-[110px]"/>
-            </div>
-            <div className="absolute md:top-[30rem] top-[12rem] md:left-[30rem] left-[8rem]">
-              <Circle className="md:w-[257px] w-[75px] md:h-[257px] h-[75px]"/> 
-            </div>
-            <div className="absolute md:-top-[5rem] top-[1rem] md:-left-[20rem] right-[2rem]">
-              <Circle className="md:w-[210px] w-[60px] md:h-[210px] h-[60px]"/> 
-            </div>
-            <div className="absolute md:top-[8rem] top-[5rem] md:left-[24rem] left-[8rem]">
-              <Circle className="md:w-[160px] w-[48px] md:h-[160px] h-[48px]"/>
-            </div>
-            <div className="absolute md:top-[22rem] top-[9rem] md:left-[23rem] left-[7rem]">
-              <Circle className="md:w-[105px] w-[30px] md:h-[105px] h-[30px]"/> 
-            </div>
-            <div className="absolute md:top-[32rem] top-[14rem] md:-right-[3rem] right-[0rem]">
-              <Circle variant="teal" className="md:w-[300px] w-[105px] md:h-[300px] h-[105px]"/>
-            </div>
-            <div className="absolute md:top-[18rem] top-[10rem] md:-left-[34rem] -left-[10rem]">
-              <Circle variant="teal" className="md:w-[200px] w-[56px] md:h-[200px] h-[56px]" /> 
-            </div>
-            <div className="absolute md:-top-[3rem] top-[5.5rem] md:-left-[32rem] -left-[11rem]">
-              <Circle variant="teal" className="md:w-[130px] w-[37px] md:h-[130px] h-[37px]"/> 
-            </div>
-          </div>
+        {/* Background Circles */}
+        <VisionCircles />
+
         <div
           id="Our_Vision"
           className="bg-white md:rounded-[5rem] rounded-[1rem] border border-black border-opacity-30 md:flex md:justify-between md:px-24 px-5 py-10 bg-opacity-20 md:backdrop-blur-[10px] backdrop-blur-[5px] md:max-w-[1175px] min-w-[280px] w-full justify-center"
