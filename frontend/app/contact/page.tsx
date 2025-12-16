b1a4dc522198d81dc23112e4ff0ec01b1184d329
renz0826 <brenzbedonia@gmail.com>
2025-12-16 21:21:05 +0800

    feat: contact page desktop view

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+import Contact_Info from "@/components/sections/Contact_Info";
+
 export default function Contact() {
   return (
-    <div>
-      <h1 className="text-primary-dark text-4xl font-bold">
-        Hello from Contact Page!
+    <section className="flex flex-col gap-20">
+      <h1 className="lg:text-6xl md:text-5xl text-4xl text-center gap-3 ">
+        <span className="text-[#152731] font-medium "> Go ahead and </span>
+        <span className="font-bold bg-gradient-to-r from-[#152731] to-[#0AAB80] bg-clip-text text-transparent">
+          Connect With Us!
+        </span>
       </h1>
-    </div>
+
+      {/* Contact Info */}
+      <Contact_Info />
+
+      {/* Map Location */}
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245.01789493495406!2d122.56025095594048!3d10.71238511361936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aee500383c9f99%3A0x7d4bdd7ea220dd6a!2sBINHI!5e0!3m2!1sen!2sph!4v1765890799005!5m2!1sen!2sph"
+        width="1175"
+        height="640"
+        allowFullScreen={true}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="flex mx-auto rounded-3xl mb-32"
+      />
+    </section>
   );
 }
---
+++ frontend/components/sections/Contact_Info.tsx
@@ -0,0 +1,163 @@
+import {
+  AcademicCapIcon,
+  ClockIcon,
+  PhoneIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/24/solid";
+
+import { ClipboardIcon } from "@heroicons/react/24/outline";
+
+import ReactClipboard from "react-clipboardjs-copy";
+
+import Forms from "@/components/ui/Contact_Form";
+
+export default function Contact_Info() {
+  return (
+    <section
+      id="Contact_Info"
+      className="grid grid-cols-2 gap-24 max-w-[1175px] mx-auto"
+    >
+      <div className="flex flex-col lg:gap-10">
+        {/* Address */}
+        <div className="flex gap-5">
+          <AcademicCapIcon
+            width={28}
+            height={24}
+            color="#152731"
+            className="mt-[.250rem]"
+          />
+          <div className="flex flex-col">
+            <h3 className="lg:text-xl md:text-lg text-base text-[#152731] lg:leading-relaxed leading-snug">
+              <span className="font-bold bg-gradient-to-r from-[#152731] to-[#0AAB80] bg-clip-text text-transparent">
+                West Visayas State University - Main Campus
+              </span>
+              <br></br>
+              <span className="font-medium">Luna St. La Paz, Iloilo City</span>
+            </h3>
+          </div>
+        </div>
+
+        {/* Hours */}
+        <div className="flex gap-5">
+          <ClockIcon
+            width={28}
+            height={24}
+            color="#152731"
+            className="mt-[.250rem]"
+          />
+          <div className="flex flex-col">
+            <h3 className="lg:text-xl md:text-lg text-base text-[#152731] lg:leading-relaxed leading-snug">
+              <span className="font-bold bg-gradient-to-r from-[#152731] to-[#0AAB80] bg-clip-text text-transparent">
+                Office Hours
+              </span>
+              <br></br>
+              <span className="font-medium">
+                Monday - Friday <br></br> 8 AM - 5 PM{" "}
+              </span>
+            </h3>
+          </div>
+        </div>
+
+        {/* Number */}
+        <div className="flex gap-5">
+          <PhoneIcon
+            width={28}
+            height={24}
+            color="#152731"
+            className="mt-[.250rem]"
+          />
+          <div className="flex flex-col">
+            <h3 className="lg:text-xl md:text-lg text-base text-[#152731] lg:leading-relaxed leading-snug">
+              <span className="font-bold bg-gradient-to-r from-[#152731] to-[#0AAB80] bg-clip-text text-transparent">
+                Office Hours
+              </span>
+              <br></br>
+              <span className="font-medium">
+                <ReactClipboard
+                  text="Text Copied"
+                  onSuccess={() => {}}
+                  onError={() => {}}
+                >
+                  <button
+                    onClick={() => {
+                      console.log("0999 999 999");
+                    }}
+                    className="flex gap-2 font-medium items-center"
+                  >
+                    0999 999 999
+                    <ClipboardIcon
+                      width={16}
+                      height={16}
+                      color="#152731"
+                      className="opacity-60"
+                    />
+                  </button>
+                </ReactClipboard>
+                <ReactClipboard
+                  text="Text Copied"
+                  onSuccess={() => {}}
+                  onError={() => {}}
+                >
+                  <button
+                    onClick={() => {
+                      console.log("0988 888 888");
+                    }}
+                    className="flex gap-2 font-medium items-center"
+                  >
+                    0988 888 888
+                    <ClipboardIcon
+                      width={16}
+                      height={16}
+                      color="#152731"
+                      className="opacity-60"
+                    />
+                  </button>
+                </ReactClipboard>
+              </span>
+            </h3>
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className="flex gap-5">
+          <EnvelopeIcon
+            width={28}
+            height={24}
+            color="#152731"
+            className="mt-[.250rem]"
+          />
+          <div className="flex flex-col">
+            <h3 className="lg:text-xl md:text-lg text-base text-[#152731] lg:leading-relaxed leading-snug">
+              <span className="font-bold bg-gradient-to-r from-[#152731] to-[#0AAB80] bg-clip-text text-transparent">
+                Email Address
+              </span>
+              <br></br>
+              <ReactClipboard
+                text="Text Copied"
+                onSuccess={() => {}}
+                onError={() => {}}
+              >
+                <button
+                  onClick={() => {
+                    console.log("spark.hub@wvsu.edu.ph");
+                  }}
+                  className="flex gap-2 font-medium items-center"
+                >
+                  spark.hub@wvsu.edu.ph
+                  <ClipboardIcon
+                    width={16}
+                    height={16}
+                    color="#152731"
+                    className="opacity-60"
+                  />
+                </button>
+              </ReactClipboard>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <Forms />
+    </section>
+  );
+}
