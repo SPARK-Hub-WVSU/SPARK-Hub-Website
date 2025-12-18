1438bc2d927f79073d25f16331063dfc56a96723
John Manuel Carado <carado.johnp11q@gmail.com>
2025-12-19 00:58:39 +0800

    Add initial initiatives data

---
+++ frontend/data/initiatives.ts
@@ -0,0 +1,41 @@
+export const initiatives = [
+  {
+    title: "BuzzEx",
+    caption:
+      "WVSU's BuzzEx Event Celebrates Success in Bridging Academia and Industry",
+    description:
+      "WVSU - SPARK Hub, alongside WVSU - BINHI, extends profound gratitude to every participant and stakeholder of BuzzEx: SPARKING Innovation with Industry BREAKTHROUGHS, spearheaded by Hsien-na Kuo, Neil Clarence Diaz, Dr. Joel De Castro, and Mr. Mark Peraman. On April 12, 2024, BuzzEx unfolded at the Function Room 4th Floor, Research and Extensions Building, WVSU, with keynote speakers representing esteemed government agencies: Engr. Rowen R. Gelonga from DOST, Enp. Roxanne C. Yap from NEDA, and Jose Francisco Gerona from PCCI - Iloilo Chapter, Inc. Distinguished panelists, including Mary Jade R. Gonzales from DTI, Marvin L. Manuel from DICT, and representatives from DA and DICT Western Visayas Regional Office, further enriched the discourse. Moreover, exhibitors like Paul Kaldi, Cyb Robotics Organization , Spark Lab, YM Lab, and Chargee by Charlie Co. showcased groundbreaking innovations, illuminating their potential applications across sectors and reinforcing the vital link between academic research and industry demands. In addition, our heartfelt appreciation extends to our media partners - Forum-Dimensions, LINK.exe, The Innovator, and ICON Publication - and partner organizations, including CICT Student Council, College of Business & Management Student Council - WVSU Arts and Sciences Student Council, Inc. and Chroma. We are deeply grateful for your invaluable contributions and underscore the significance of such gatherings in nurturing dialogue and collaboration between academia and industry. Together, we continue to ignite innovation and drive transformative change. Photo Courtesy of LINK.exe",
+    image: "/images/buzzex.webp",
+  },
+  {
+    title: "Training Odyssey",
+    caption:
+      "Training Odyssey 2025 is your blueprint for turning dreams into ventures",
+    description:
+      "Your blueprint for Growth: Training Odyssey 2025. Training Odyssey 2025 is your blueprint for turning dreams into ventures! This event is designed to bridge the gap between your visionary ideas and the practical skills needed to bring them to life. With expert-led sessions, interactive workshops, and mock pitching opportunities, you’ll gain practical insights into creating business models, crafting value propositions, and presenting to real-world professionals. This is the place to learn, connect, and grow as a technopreneur.",
+    image: "/images/to2025.webp",
+  },
+  {
+    title: "SeedSpark",
+    caption: "SeedSpark Technology Pitch Fest 2025 — Where Big Ideas Take Root",
+    description:
+      "SeedSpark Technology Pitch Fest 2025 calls YOU to pitch, innovate, and make an impact. More than just a pitch competition, it’s the grand finale of the IP IGNITE Series — a dynamic launchpad where WVSU students from all campuses grow their boldest ideas into real-world ventures. Powered by the Wadhwani Foundation’s Ignite Platform, you’ll gain access to startup learning modules, receive expert mentorship, and be equipped with everything you need to perfect your pitch. This is your opportunity to be part of an exclusive venture-building program, setting you on the path to becoming the next successful startup founder.",
+    image: "/images/seedspark.webp",
+  },
+  {
+    title: "Real Time Digital Point System Tracker",
+    caption:
+      "This tracker keeps you connected—track standings, view scores, and follow every merit and demerit earned by your curriculum!",
+    description:
+      "Are you ready to witness the thrill and excitement unfold in real time? Stay updated with CBM FUNDAYS 2024! Our real-time digital point system tracker keeps you connected—track standings, view scores, and follow every merit and demerit earned by your curriculum!",
+    image: "/images/tracker.webp",
+  },
+  {
+    title: "Training Odyssey 2023",
+    caption:
+      "Fuel Your Expertise: Spark Hub's Marketing-Finance & Project Management Odyssey",
+    description:
+      "Join us on a transformative journey as Spark Hub unveils the third phase of the Training Odyssey. Illuminate profound insights by participating in the upcoming Spark Hub Marketing and Finance Training on November 24, Friday, at 1:00 P.M. and the discussion about the Foundation of Project Management on November 25, Saturday, via Google Meet! This session aims to equip you with the skills and knowledge needed for dynamic learning. Explore the marketing realm as we delve into processes such as developing effective strategies, promotional tactics, and brand-building techniques that resonate in today's competitive landscape. Immerse yourself in finance as we unravel how the promising committee makes sound financial decisions. Dive into the mystique of managing projects and its different structure and systematic processes . We're excited to help you uncover your latent knowledge and skills! This training leverages data to generate effective marketing strategies, offering a comprehensive awareness of the financial landscape, and developing core tools with different techniques for project management. Prepare to acquire a robust skill set that will enable you to thrive in marketing, finance, and project management. Expand your horizons and ignite your potential with SparkHub's Odyssey: Marketing, Finance, and Project Management Training—where expertise sparks innovation. See you there, Sparknovators!",
+    image: "/images/to2023.webp",
+  },
+];
