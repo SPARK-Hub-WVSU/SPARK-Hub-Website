162dbab8a49ec6b4276a6b22cfcab384b71a8c93
John Manuel Carado <carado.johnp11q@gmail.com>
2025-12-24 20:13:25 +0800

    feat(about): Develop responsive and interactive founders section

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
 import PrincipleCard from "@/components/ui/PrincipleCard";
 import { ArticleCard, ImageCard } from "@/components/ui/InitiativeCards";
 import { initiatives } from "@/data/initiatives";
+import Founding from "@/components/ui/Founding";
+import { useState } from "react";
+import { foundingTeam } from "@/data/founding";
 export default function About() {
+  const [activeFounder, setActiveFounder] = useState(foundingTeam[0]);
   return (
     <>
       <section className="bg-primary-dark bg-[url('/bg/officers_bg.webp')] bg-cover bg-center bg-no-repeat sm:min-h-screen lg:-mt-30 -mt-40 px-3">
@@ -80,7 +85,6 @@ export default function About() {
           </div>
         </div>
       </section>
-
       <section className="flex md:flex-row flex-col-reverse md:gap-x-5 gap-x-0 sm:gap-y-5 min-h-screen md:px-8 px-3 py-10 justify-between">
         <div className="h-full  w-full flex flex-col gap-y-3">
           {initiatives.map((item, i) => {
@@ -131,6 +135,49 @@ export default function About() {
           <small className="text-end text-xl">
             Empower Ideas <br /> into Action
           </small>
+        </div>
+      </section>
+
+      <section className="flex flex-col items-center w-full min-h-[36rem] md:bg-gradient-to-br bg-gradient-to-b from-yellow-200 from-5% via-transparent via-70% to-primary-light md:pt-10 pt-5 py-10 md:py-0 md:gap-y-0 gap-y-5">
+        {/* TOP DETAILS */}
+        <div className="flex md:flex-row flex-col-reverse gap-x-20 justify-center w-full md:gap-y-0 gap-y-5">
+          {/* DEETS */}
+          <div className="md:px-0 px-5 md:gap-y-5 gap-y-3 flex flex-col md:w-1/2 md:items-start items-center">
+            <p className="font-bold md:text-7xl text-2xl text-primary">
+              {activeFounder.name}
+            </p>
+
+            <p className="font-bold md:text-xl text-base text-primary tracking-wide md:text-start text-center">
+              {activeFounder.title}
+            </p>
+
+            <p className="md:text-lg text-sm md:tracking-wide tracking-tight text-justify">
+              {activeFounder.summary}
+            </p>
+          </div>
+
+          {/* IMAGE */}
+          <div className="bg-[url('/bg/founders_bg.webp')] bg-no-repeat bg-contain relative md:h-[40rem] h-[24rem] md:w-[32rem] w-full flex justify-center">
+            <Image
+              src={activeFounder.imageSrc}
+              alt={activeFounder.name}
+              fill
+              style={{ objectFit: "contain" }}
+              priority
+            />
+          </div>
+        </div>
+
+        {/* FOUNDERS GRID */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-x-36 justify-items-center items-center md:w-full w-10/12 min-h-[12rem] md:h-36 p-6 md:p-0 bg-[#378394]/50 md:rounded-none rounded-2xl">
+          {foundingTeam.map((member) => (
+            <Founding
+              key={member.name}
+              name={member.name}
+              imageSrc={member.imageSrc}
+              onClick={() => setActiveFounder(member)}
+            />
+          ))}
         </div>
       </section>
     </>
