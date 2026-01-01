446c59effd3772ad7b6718e8c43f4f301fea8842
Christopher-Glenn <christopherglenn.bedis@wvsu.edu.ph>
2026-01-01 20:25:28 +0800

    fix: resolve conflicts

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const pathname = usePathname();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const isDarkHeader = pathname.startsWith("/events") || pathname.startsWith("/contact")
+  const isDarkHeader = pathname.startsWith("/events") || pathname.startsWith('/contact')
     ;
   return (
     <header className="bg-transparent relative z-10">
