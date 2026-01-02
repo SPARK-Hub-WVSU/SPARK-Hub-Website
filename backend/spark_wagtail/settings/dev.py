32cb5bff86a87480ba973ff44cdd1a0de9a3107b
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-02 10:14:29 +0800

    feat(backend): Initialize Wagtail & PostgreSQL with uv

---
+++ backend/spark_wagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ax3ag!7-##qj(q@dee9(-q0-0veapbfb)$6o)8!%b+*beyfy_m"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
