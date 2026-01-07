1e7ee5a7b36572c68625a3753d0893daaf341e60
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-07 21:21:05 +0800

    refactor(header): Make code readable and clean

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -10,84 +10,81 @@ export default function Header() {
   const pathname = usePathname();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const isDarkHeader = pathname.startsWith("/events") || pathname.startsWith('/contact')
-    ;
+  const isDarkHeader =
+    pathname.startsWith("/events") || pathname.startsWith("/contact");
+
+  const textColor = isDarkHeader ? "text-metallic" : "text-white";
+  const logoWrapperBg = isDarkHeader
+    ? "bg-gradient-to-b from-black via-teal-950 to-cyan-700"
+    : "bg-transparent";
+
   return (
-    <header className="bg-transparent relative z-10">
-      <nav className="text-white text-2xl lg:text-2xl md:text-xl sm:text-lg w-full h-auto flex flex-row items-center justify-between md:py-3 sm:px-12 px-6 md:px-16 lg:px-36 sm:my-7 my-5">
+    <header className="relative z-10 bg-transparent">
+      <nav className="w-full flex items-center justify-between text-white md:py-3 sm:px-12 px-6 md:px-16 lg:px-36 sm:my-7 my-5">
         {/* Logo */}
-        <div id="Home" className="mb-2">
-          <Link
-            href="/"
-            className="font-semibold text-xl sm:text-2xl md:text-2xl lg:text-3xl flex items-center gap-4 sm:gap-6 md:gap-8 relative"
+        <Link
+          href="/"
+          className="flex items-center gap-4 sm:gap-6 md:gap-8 relative"
+        >
+          <div className={`rounded-xl overflow-hidden ${logoWrapperBg}`}>
+            <Image
+              src="/vectors/nav_logo_dark.svg"
+              alt="SPARK Hub Logo"
+              width={45}
+              height={45}
+              priority
+              className="pt-2 px-1 w-9 h-9 sm:w-12 sm:h-12 xl:w-16 xl:h-16"
+            />
+          </div>
+
+          <h1
+            className={`hidden sm:block tracking-wide font-semibold text-xl sm:text-2xl md:text-2xl lg:text-3xl ${textColor}`}
           >
-            <div
-              className={` ${isDarkHeader
-                  ? "bg-gradient-to-b from-black via-teal-950 to-cyan-700 rounded-xl overflow-hidden"
-                  : ""
+            SPARK HUB
+          </h1>
+        </Link>
+
+        {/* Desktop Navigation */}
+        <div
+          className={`hidden md:flex items-center gap-4 lg:gap-10 font-medium tracking-wide ${textColor}`}
+        >
+          {["Events", "About Us", "Team"].map((label, i) => {
+            const href =
+              label === "Events"
+                ? "/events"
+                : label === "About Us"
+                  ? "/about"
+                  : "/officers";
+
+            return (
+              <Link
+                key={i}
+                href={href}
+                className="relative group text-sm md:text-base lg:text-xl transition-all duration-300"
+              >
+                <span className="relative z-10">{label}</span>
+                <span className="absolute left-0 bottom-[-0.40rem] w-full h-0.5 bg-accent scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center" />
+              </Link>
+            );
+          })}
+
+          {/* Contact Button */}
+          <Link href="/contact" className="hover:scale-105 transition-all">
+            <button
+              className={`flex items-center gap-2 lg:gap-3 px-3 py-2 md:px-5 md:py-3 lg:px-7 lg:py-5 rounded-2xl lg:rounded-3xl border text-sm md:text-base lg:text-xl backdrop-blur-3xl
+                ${
+                  isDarkHeader
+                    ? "bg-metallic text-white border-[#1DE5B0]"
+                    : "bg-white bg-opacity-20 text-accent"
                 }`}
             >
-              <Image
-                src={
-                  isDarkHeader
-                    ? "/vectors/nav_logo_dark.svg"
-                    : "/vectors/nav_logo.svg"
-                }
-                alt="SPARK Hub Logo"
-                width={45}
-                height={45}
-                priority
-                className="pt-2 px-1 w-9 h-9 sm:w-12 sm:h-12 xl:w-16 xl:h-16"
+              <span>Contact Us</span>
+              <ArrowRightIcon
+                strokeWidth={4}
+                className={`h-4 md:h-5 lg:h-6 ${
+                  isDarkHeader ? "stroke-white" : "stroke-accent"
+                }`}
               />
-            </div>
-
-            <h1 className={`hidden sm:block tracking-wide
-              ${isDarkHeader ? "text-metallic" : "text-white"}`}>SPARK HUB</h1>
-          </Link>
-        </div>
-
-        {/* Desktop Navigation Links */}
-        <div
-          id="Content"
-          className={`hidden md:flex flex-row gap-4 lg:gap-10 justify-center items-center font-medium tracking-wide
-            ${isDarkHeader ? "text-metallic" : "text-white"}`}
-        >
-          <Link
-            href="/events"
-            className="transform transition-all duration-300 ease-in-out text-sm md:text-base lg:text-xl relative group"
-          >
-            <span className="relative z-10">Events</span>
-            <span className="absolute left-0 bottom-[-0.40rem] w-full h-0.5 bg-accent scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center" />
-          </Link>
-          <Link
-            href="/about"
-            className="transform transition-all duration-300 ease-in-out text-sm md:text-base lg:text-xl relative group"
-          >
-            <span className="relative z-10">About Us</span>
-            <span className="absolute left-0 bottom-[-0.40rem] w-full h-0.5 bg-accent scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center" />
-          </Link>
-          <Link
-            href="/officers"
-            className="transform transition-all duration-300 ease-in-out text-sm md:text-base lg:text-xl relative group"
-          >
-            <span className="relative z-10">Team</span>
-            <span className="absolute left-0 bottom-[-0.40rem] w-full h-0.5 bg-accent scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center" />
-          </Link>
-          <Link
-            id="Contact"
-            href="/contact"
-            className="transform transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-105"
-          >
-            <button className={`bg-opacity-5 backdrop-blur-3xl w-auto px-3 py-2 md:px-5 md:py-3 lg:px-7 lg:py-5 flex flex-row items-center gap-2 lg:gap-3 border rounded-2xl lg:rounded-3xl text-sm md:text-base lg:text-xl
-              ${isDarkHeader ? "text-white bg-metallic border-[#1DE5B0]" : "text-accent bg-white"}`}>
-              <div>Contact Us</div>
-              <div>
-                <ArrowRightIcon
-                  strokeWidth={4}
-                  className={`mx-1 h-4 md:h-5 lg:h-6 
-                    ${isDarkHeader ? "stroke-white" : "stroke-accent"}`}
-                />
-              </div>
             </button>
           </Link>
         </div>
@@ -95,13 +92,12 @@ export default function Header() {
         {/* Mobile Menu Button */}
         <button
           className="md:hidden"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          onClick={() => setIsMobileMenuOpen((v) => !v)}
           aria-label="Toggle mobile menu"
         >
           {isMobileMenuOpen ? (
-            <XMarkIcon className="text-black h-6 w-6" />
+            <XMarkIcon className="h-6 w-6 text-black" />
           ) : (
-            // <Bars3Icon className="h-6 w-6" />
             <Image
               src={
                 isDarkHeader
@@ -110,45 +106,32 @@ export default function Header() {
               }
               width={42}
               height={42}
-              alt="hamburger icon"
+              alt="menu"
               className="shadow-xl"
             />
           )}
         </button>
       </nav>
 
-      {/* Mobile Navigation Menu */}
+      {/* Mobile Menu */}
       {isMobileMenuOpen && (
         <div className="md:hidden bg-white shadow-xl">
           <div className="px-4 py-7 space-y-4">
-            <Link
-              href="/events"
-              className="block text-black hover:text-accent transition-colors duration-300 text-lg"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Events
-            </Link>
-            <Link
-              href="/about"
-              className="block text-black hover:text-accent transition-colors duration-300 text-lg"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              About Us
-            </Link>
-            <Link
-              href="/officers"
-              className="block text-black hover:text-accent transition-colors duration-300 text-lg"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Officers
-            </Link>
-            <Link
-              href="/contact"
-              className="block text-black hover:text-accent transition-colors duration-300 text-lg"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Contact Us
-            </Link>
+            {[
+              { label: "Events", href: "/events" },
+              { label: "About Us", href: "/about" },
+              { label: "Officers", href: "/officers" },
+              { label: "Contact Us", href: "/contact" },
+            ].map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="block text-black hover:text-accent transition-colors text-lg"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                {item.label}
+              </Link>
+            ))}
           </div>
         </div>
       )}
