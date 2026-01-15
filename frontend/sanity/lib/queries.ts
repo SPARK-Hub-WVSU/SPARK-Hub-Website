4372d568267d58600db9950d5bcf326865d35225
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-15 19:49:21 +0800

    Add GROQ queries

---
+++ frontend/sanity/lib/queries.ts
@@ -0,0 +1,23 @@
+import { defineQuery } from "next-sanity";
+
+export const POSTS_QUERY =
+  defineQuery(`*[_type == "post"] | order(publishedAt desc) {
+  "id": slug.current,
+  title,
+  excerpt,
+  "date": publishedAt,
+  tags,
+  image,
+  content
+}`);
+
+export const POST_BY_SLUG_QUERY =
+  defineQuery(`*[_type == "post" && slug.current == $slug][0] {
+  "id": slug.current,
+  title,
+  excerpt,
+  "date": publishedAt,
+  tags,
+  image,
+  content
+}`);
