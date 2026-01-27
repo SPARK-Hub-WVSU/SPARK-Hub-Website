5fb2e67aef3707a9a5d1dc499fe42b34fa06b78c
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-27 23:24:59 +0800

    refactor: Footer and About label to founding members

--- components/sections/Founders.tsx
+++ components/sections/Founders.tsx
@@ -11,7 +11,7 @@ export default function FoundersSection() {
 
   return (
     <section
-      id="testimonials"
+      id="foundingTeam"
       className="flex flex-col items-center w-full min-h-[36rem] md:bg-gradient-to-br bg-gradient-to-b from-yellow-200 from-5% via-transparent via-70% to-primary-light md:pt-10 pt-5 py-10 md:py-0 md:gap-y-0 gap-y-5"
     >
       <div className="flex md:flex-row flex-col-reverse gap-x-20 justify-center w-full md:gap-y-0 gap-y-5">
